496edaf29e7fe6ea2aa8cab26c4d3900077d63ab
ayushpasi <pasiayush999@gmail.com>
2023-12-23 22:16:20 +0530

    working with union types

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
     console.log(result);
+    console.log(stringResult);
 });
 function add(a, b) {
-    return a + b;
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    if (typeof a === "string" && typeof b === "string") {
+        return a + " " + b;
+    }
+    return +a + +b;
 }
--- app.ts
+++ app.ts
@@ -6,9 +6,17 @@ buttonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
+  const stringResult = add(num1, num2);
   console.log(result);
+  console.log(stringResult);
 });
 
-function add(a: number, b: number): number {
-  return a + b;
+function add(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  if (typeof a === "string" && typeof b === "string") {
+    return a + " " + b;
+  }
+  return +a + +b;
 }
