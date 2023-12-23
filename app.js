414e3e1eb9246686732c13509d33cb8dbfc1fb5e
ayushpasi <pasiayush999@gmail.com>
2023-12-23 23:49:29 +0530

    using objects and array

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button");
+const numResult = [];
+const textResult = [];
 buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
     const stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    printResult({ val: result, timeStamp: new Date() });
+    numResult.push(result);
+    textResult.push(stringResult);
+    console.log(numResult, textResult);
 });
+function printResult(resObj) {
+    console.log(resObj.val);
+    console.log(resObj.timeStamp);
+}
 function add(a, b) {
     if (typeof a === "number" && typeof b === "number") {
         return a + b;
--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
-
+const numResult: number[] = [];
+const textResult: string[] = [];
 buttonElement.addEventListener("click", () => {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
   const result = add(+num1, +num2);
   const stringResult = add(num1, num2);
-  console.log(result);
-  console.log(stringResult);
+  printResult({ val: result as number, timeStamp: new Date() });
+  numResult.push(result as number);
+  textResult.push(stringResult as string);
+  console.log(numResult, textResult);
 });
-
+function printResult(resObj: { val: number; timeStamp: Date }) {
+  console.log(resObj.val);
+  console.log(resObj.timeStamp);
+}
 function add(a: number | string, b: number | string) {
   if (typeof a === "number" && typeof b === "number") {
     return a + b;
